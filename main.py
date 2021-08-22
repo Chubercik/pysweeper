0f0c3ef049b45fbb1b9ab1c211a4fa622dd294ee
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2021-08-22 23:56:09 +0200

    add proof-of-concept create_a_board() and scatter_bombs() functions

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+from random import randint
+
+
+def create_a_board(width, heigth):
+    board = []
+    for line in range(heigth):
+        board.append([])
+        for _ in range(width):
+            board[line].append(0)
+    return board
+
+
+def scatter_bombs(board):
+    for line in range(len(board)):
+        for tile in range(len(board[line])):
+            rand_num = randint(1, 100)
+            if 75 < rand_num <= 100:
+                board[line][tile] = 'X'
+
+
 def main():
-    print("Hello World!")
+    board = create_a_board(5, 4)
+    scatter_bombs(board)
+    print(board)
 
 
 if __name__ == "__main__":
