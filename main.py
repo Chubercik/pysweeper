575e9c267f6d24086f86011235b821b2331bfb94
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2021-08-24 01:39:03 +0200

    first working version of graph generation

--- main.py
+++ main.py
@@ -1,27 +1,156 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
 from random import randint
+from typing import List, Optional
 
 
-def create_a_board(width, heigth):
-    board = []
-    for line in range(heigth):
-        board.append([])
-        for _ in range(width):
-            board[line].append(0)
-    return board
+@dataclass
+class Position:
+    x: int
+    y: int
+
+    def neighbours(self, bounds: Optional[Rectangle] = None):
+        """
+        Yields all neighbouring positions which fit in the given rectangle.
+        """
+        d = [-1, 1]
+        for dx in d:
+            pos = Position(self.x + dx, self.y)
+            if bounds is None or pos in bounds:
+                yield pos
+        for dy in d:
+            pos = Position(self.x, self.y + dy)
+            if bounds is None or pos in bounds:
+                yield pos
+
+        for dx in d:
+            for dy in d:
+                pos = Position(self.x + dx, self.y + dy)
+                if bounds is None or pos in bounds:
+                    yield pos
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
 
 
-def scatter_bombs(board):
-    for line in range(len(board)):
-        for tile in range(len(board[line])):
-            rand_num = randint(1, 100)
-            if 75 < rand_num <= 100:
-                board[line][tile] = 'X'
+@dataclass(order=True)
+class Point:
+    value: int = field(default=0)
+    bomb: bool = field(default=False)
+    pos: Position = field(default=Position(0, 0))
+
+    def __str__(self):
+        return '{' + f"v: {self.value}, b: {self.bomb}" + '}'
 
 
-def main():
-    board = create_a_board(5, 4)
-    scatter_bombs(board)
-    print(board)
+@dataclass
+class Rectangle:
+    _bl: Position   # bottom left vertex
+    _tr: Position   # top right vertex
+
+    def __post_init__(self):
+        if self._bl.x > self._tr.x:
+            tmp = self._bl.x
+            self._bl.x = self._tr.x
+            self._tr.x = tmp
+        if self._bl.y > self._tr.y:
+            tmp = self._bl.y
+            self._bl.y = self._tr.y
+            self._tr.y = tmp
+
+    def __contains__(self, position: Position) -> True:
+        """
+        Checks if a position fits inside the rectangle.
+        """
+        return position.x >= self._bl.x and position.y >= self._bl.y and \
+            position.x <= self._tr.x and position.y <= self._tr.y
+
+
+class Graph:
+    """
+    Class representing a 2D graph.
+    """
+
+    def __init__(self, height: int, width: int) -> None:
+        """
+        Initializes a Graph object.
+        """
+
+        self._height = height
+        self._width = width
+        self._bounds = Rectangle(
+            Position(0, 0), Position(self._width-1, self._height-1))
+
+        self._graph: List[List[Point]] = []
+        for y in range(height):
+            graph_line: List[Point] = []
+            for x in range(self._width):
+                graph_line.append(Point(0, False, Position(x, self._height - 1 - y)))
+            self._graph.append(graph_line)
+
+    def _get(self, position: Position) -> Point:
+        """
+        Returns a point given the position.
+        """
+        return self._graph[(self._height - position.y - 1)][(position.x)]
+
+    def print_graph(self) -> str:
+        graph_str = ""
+        for line in self._graph:
+            for point in line:
+                graph_str += f"{str(point.value)} "
+            graph_str = graph_str[:-1]
+            graph_str += "\n"
+        graph_str = graph_str[:-1]
+        return graph_str
+
+    def print_bombs(self) -> str:
+        graph_str = ""
+        for line in self._graph:
+            for point in line:
+                graph_str += "1 " if point.bomb else "0 "
+            graph_str = graph_str[:-1]
+            graph_str += "\n"
+        graph_str = graph_str[:-1]
+        return graph_str
+
+    def print_pos(self) -> str:
+        graph_str = ""
+        for line in self._graph:
+            for point in line:
+                graph_str += f"{str(point.pos)} "
+            graph_str = graph_str[:-1]
+            graph_str += "\n"
+        graph_str = graph_str[:-1]
+        return graph_str
+
+    def scatter_bombs(self) -> None:
+        for line in self._graph:
+            for point in line:
+                rand_num = randint(0, 9)
+                if rand_num == 7:
+                    point.value = 'X'
+                    point.bomb = True
+                    for neighbour in point.pos.neighbours(self._bounds):
+                        if str(self._get(neighbour).value).isdigit() is True:
+                            self._get(neighbour).value += 1
+
+
+def main() -> None:
+    graph = Graph(10, 10)
+    graph.scatter_bombs()
+    print(graph.print_graph())
+    print()
+    print(graph.print_bombs())
+    print()
+    print(graph.print_pos())
 
 
 if __name__ == "__main__":
