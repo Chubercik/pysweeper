74209dcddea59928518d6a40dbccdad303b64867
Jakub Marcowski <01158831@pw.edu.pl>
2022-02-16 20:13:52 +0100

    Revisit the repository and refactor some code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from time import perf_counter
 from dataclasses import dataclass, field
 from random import randint
 from typing import List, Optional
@@ -8,12 +7,23 @@ from typing import List, Optional
 
 @dataclass
 class Position:
+    """
+    Class representing a position in a 2D grid.
+
+    Attributes:
+        x: The x-coordinate of the position.
+        y: The y-coordinate of the position.
+
+    Methods:
+        neighbours: Returns all (8) neighbouring positions
+        which fit in the given rectangle.
+    """
     x: int
     y: int
 
-    def neighbours(self, bounds: Optional[Rectangle] = None):
+    def neighbours(self, bounds: Optional[Rectangle] = None) -> None:
         """
-        Yields all neighbouring positions which fit in the given rectangle.
+        Yields all (8) neighbouring positions which fit in the given rectangle.
         """
         d = [-1, 1]
         for dx in d:
@@ -31,24 +41,23 @@ class Position:
                 if bounds is None or pos in bounds:
                     yield pos
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"({self.x}, {self.y})"
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
 
-    def __hash__(self):
-        return hash((self.x, self.y))
+@dataclass
+class Field:
+    """
+    Class representing a Minesweeper field.
 
-
-@dataclass(order=True)
-class Point:
+    Attributes:
+        value: The value of the field.
+        bomb: Whether the field contains a bomb.
+        pos: The position of the field.
+    """
     value: int = field(default=0)
     bomb: bool = field(default=False)
     pos: Position = field(default=Position(0, 0))
-
-    def __str__(self):
-        return '{' + f"v: {self.value}, b: {self.bomb}" + '}'
 
 
 @dataclass
@@ -56,7 +65,7 @@ class Rectangle:
     _bl: Position   # bottom left vertex
     _tr: Position   # top right vertex
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if self._bl.x > self._tr.x:
             tmp = self._bl.x
             self._bl.x = self._tr.x
@@ -66,7 +75,7 @@ class Rectangle:
             self._bl.y = self._tr.y
             self._tr.y = tmp
 
-    def __contains__(self, position: Position) -> True:
+    def __contains__(self, position: Position) -> bool:
         """
         Checks if a position fits inside the rectangle.
         """
@@ -74,85 +83,106 @@ class Rectangle:
             position.x <= self._tr.x and position.y <= self._tr.y
 
 
-class Graph:
+class Board:
     """
-    Class representing a 2D graph.
+    Class representing a game board.
+
+    Attributes:
+        height: The height of the board.
+        width: The width of the board.
+        board: The board itself.
+        bounds: The bounds of the board.
+
+    Methods:
+        get_field: Returns a field given the position.
+        scatter_bombs: Scatters bombs on the board.
+        print_board: Prints the board.
+        print_bombs: Prints the bombs.
+        print_pos: Prints the positions.
     """
 
     def __init__(self, height: int, width: int) -> None:
-        """
-        Initializes a Graph object.
-        """
-
         self._height = height
         self._width = width
         self._bounds = Rectangle(
             Position(0, 0), Position(self._width-1, self._height-1))
 
-        self._graph: List[List[Point]] = []
+        self._board: List[List[Field]] = []
         for y in range(height):
-            graph_line: List[Point] = []
-            for x in range(self._width):
-                graph_line.append(Point(0, False, Position(x, self._height - 1 - y)))
-            self._graph.append(graph_line)
+            board_line: List[Field] = [
+                Field(0, False, Position(x, self._height - 1 - y))
+                for x in range(self._width)]
 
-    def _get(self, position: Position) -> Point:
+            self._board.append(board_line)
+
+    def get_field(self, position: Position) -> Field:
         """
-        Returns a point given the position.
+        Returns a field given the position.
         """
-        return self._graph[(self._height - position.y - 1)][(position.x)]
-
-    def print_graph(self) -> str:
-        graph_str = ""
-        for line in self._graph:
-            for point in line:
-                graph_str += f"{str(point.value)} "
-            graph_str = graph_str[:-1]
-            graph_str += "\n"
-        graph_str = graph_str[:-1]
-        return graph_str
-
-    def print_bombs(self) -> str:
-        graph_str = ""
-        for line in self._graph:
-            for point in line:
-                graph_str += "1 " if point.bomb else "0 "
-            graph_str = graph_str[:-1]
-            graph_str += "\n"
-        graph_str = graph_str[:-1]
-        return graph_str
-
-    def print_pos(self) -> str:
-        graph_str = ""
-        for line in self._graph:
-            for point in line:
-                graph_str += f"{str(point.pos)} "
-            graph_str = graph_str[:-1]
-            graph_str += "\n"
-        graph_str = graph_str[:-1]
-        return graph_str
+        return self._board[(self._height - position.y - 1)][(position.x)]
 
     def scatter_bombs(self) -> None:
-        for line in self._graph:
+        """
+        Scatters bombs on the board.
+        """
+        for line in self._board:
             for point in line:
                 rand_num = randint(0, 9)
                 if rand_num == 7:
                     point.value = 'X'
                     point.bomb = True
                     for neighbour in point.pos.neighbours(self._bounds):
-                        if str(self._get(neighbour).value).isdigit() is True:
-                            self._get(neighbour).value += 1
+                        if str(self.get_field(neighbour).value).isdigit():
+                            self.get_field(neighbour).value += 1
+
+    def print_board(self) -> str:
+        """
+        Prints the board.
+        """
+        board_str = ''
+        for line in self._board:
+            for point in line:
+                board_str += f"{point.value} "
+            board_str = board_str[:-1]
+            board_str += '\n'
+        board_str = board_str[:-1]
+        return board_str
+
+    def print_bombs(self) -> str:
+        """
+        Prints the bombs.
+        """
+        board_str = ''
+        for line in self._board:
+            for point in line:
+                board_str += "1 " if point.bomb else "0 "
+            board_str = board_str[:-1]
+            board_str += '\n'
+        board_str = board_str[:-1]
+        return board_str
+
+    def print_pos(self) -> str:
+        """
+        Prints the positions.
+        """
+        board_str = ''
+        for line in self._board:
+            for point in line:
+                board_str += f"{point.pos} "
+            board_str = board_str[:-1]
+            board_str += '\n'
+        board_str = board_str[:-1]
+        return board_str
 
 
-def main() -> None:
-    start = perf_counter()
-    graph = Graph(100, 100)
-    graph.scatter_bombs()
-    stop = perf_counter()
-    print(graph.print_graph())
-    print(f"Generated the graph in {stop - start:0.4f} seconds.")
-    # It takes around 0.02s - 0.03s to generate
-    # a 100x100 graph on my setup, which is acceptable.
+def main():
+    board = Board(10, 10)
+    board.scatter_bombs()
+    print(board.print_board())
+    print()
+    print(board.print_bombs())
+    print()
+    print(board.print_pos())
 
 
 if __name__ == "__main__":
