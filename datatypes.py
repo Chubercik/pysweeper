6a798520fd0e4aa2f373799c77668f3cbb694912
Jakub Marcowski <01158831@pw.edu.pl>
2022-03-07 15:14:22 +0100

    Add more functionality (e.g. restarting after loss)

--- datatypes.py
+++ datatypes.py
@@ -8,170 +8,207 @@ sprites = Sprites().sprites
 
 class Block:
     def __init__(self) -> None:
-        self.is_bomb = False
-        self.is_exploded = False
-        self.is_flagged = False
-        self.is_question_mark = False
-        self.is_revealed = False
-        self.number = 0
-        self.x = 0
-        self.y = 0
+        self._is_bomb = False
+        self._is_exploded = False
+        self._is_flagged = False
+        self._is_question_mark = False
+        self._is_revealed = False
+        self._number = 0
+        self._x = 0
+        self._y = 0
 
     def draw(self) -> None:
         wall = pygame.Surface((32, 32))
         wall.fill((30, 30, 30))
-        screen.blit(wall, (self.x, self.y))
+        screen.blit(wall, (self._x, self._y))
         wall = pygame.transform.scale(wall, (30, 30))
         wall.fill((150, 150, 150))
-        screen.blit(wall, (self.x + 1, self.y + 1))
-        if self.is_revealed:
+        screen.blit(wall, (self._x + 1, self._y + 1))
+        if self.is_revealed():
             wall.fill((100, 100, 100))
-            screen.blit(wall, (self.x + 1, self.y + 1))
-            if self.is_exploded:
-                screen.blit(sprites["bomb_explode"], (self.x, self.y))
-            elif self.is_bomb:
-                screen.blit(sprites["bomb"], (self.x, self.y))
-            elif self.is_flagged:
-                screen.blit(sprites["bomb_no"], (self.x, self.y))
-            elif self.number == 1:
-                screen.blit(sprites["one"], (self.x, self.y))
-            elif self.number == 2:
-                screen.blit(sprites["two"], (self.x, self.y))
-            elif self.number == 3:
-                screen.blit(sprites["three"], (self.x, self.y))
-            elif self.number == 4:
-                screen.blit(sprites["four"], (self.x, self.y))
-            elif self.number == 5:
-                screen.blit(sprites["five"], (self.x, self.y))
-            elif self.number == 6:
-                screen.blit(sprites["six"], (self.x, self.y))
-            elif self.number == 7:
-                screen.blit(sprites["seven"], (self.x, self.y))
-            elif self.number == 8:
-                screen.blit(sprites["eight"], (self.x, self.y))
-        elif self.is_flagged:
-            screen.blit(sprites["flag"], (self.x, self.y))
-        elif self.is_question_mark:
-            screen.blit(sprites["question_mark"], (self.x, self.y))
+            screen.blit(wall, (self._x + 1, self._y + 1))
+            if self.is_exploded():
+                screen.blit(sprites["bomb_explode"], self.get_position())
+            elif self.is_bomb():
+                if self.is_flagged():
+                    screen.blit(sprites["flag"], self.get_position())
+                else:
+                    screen.blit(sprites["bomb"], self.get_position())
+            elif self.is_flagged():
+                screen.blit(sprites["bomb_no"], self.get_position())
+            elif self.get_number() == 1:
+                screen.blit(sprites["one"], self.get_position())
+            elif self.get_number() == 2:
+                screen.blit(sprites["two"], self.get_position())
+            elif self.get_number() == 3:
+                screen.blit(sprites["three"], self.get_position())
+            elif self.get_number() == 4:
+                screen.blit(sprites["four"], self.get_position())
+            elif self.get_number() == 5:
+                screen.blit(sprites["five"], self.get_position())
+            elif self.get_number() == 6:
+                screen.blit(sprites["six"], self.get_position())
+            elif self.get_number() == 7:
+                screen.blit(sprites["seven"], self.get_position())
+            elif self.get_number() == 8:
+                screen.blit(sprites["eight"], self.get_position())
+        elif self.is_flagged():
+            screen.blit(sprites["flag"], self.get_position())
+        elif self.is_question_mark():
+            screen.blit(sprites["question_mark"], self.get_position())
 
     def reveal(self) -> None:
-        self.is_revealed = True
+        self._is_revealed = True
 
     def flag(self) -> None:
-        self.is_flagged = True
+        self._is_flagged = True
 
     def question_mark(self) -> None:
-        self.is_question_mark = True
+        self._is_question_mark = True
 
     def unflag(self) -> None:
-        self.is_flagged = False
+        self._is_flagged = False
 
     def unquestion_mark(self) -> None:
-        self.is_question_mark = False
+        self._is_question_mark = False
 
     def set_number(self, number: int) -> None:
-        self.number = number
+        self._number = number
 
     def increase_number(self) -> None:
-        self.number += 1
+        self._number += 1
 
     def set_bomb(self) -> None:
-        self.is_bomb = True
+        self._is_bomb = True
 
     def set_exploded(self) -> None:
-        self.is_exploded = True
+        self._is_exploded = True
 
     def set_position(self, x: int, y: int) -> None:
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
 
     def is_bomb(self) -> bool:
-        return self.is_bomb
+        return self._is_bomb
 
     def is_exploded(self) -> bool:
-        return self.is_exploded
+        return self._is_exploded
 
     def is_flagged(self) -> bool:
-        return self.is_flagged
+        return self._is_flagged
 
     def is_question_mark(self) -> bool:
-        return self.is_question_mark
+        return self._is_question_mark
 
     def is_revealed(self) -> bool:
-        return self.is_revealed
+        return self._is_revealed
 
     def get_number(self) -> int:
-        return self.number
+        return self._number
 
     def get_position(self) -> Tuple[int]:
-        return (self.x, self.y)
+        return (self._x, self._y)
 
 
 class Board:
     def __init__(self, width: int, height: int, bomb_count: int) -> None:
-        self.board = []
-        self.width = width
-        self.height = height
-        self.bomb_count = bomb_count
-        for i, y in enumerate(range(0, self.height*32, 32)):
-            self.board.append([])
-            for j, x in enumerate(range(0, self.width*32, 32)):
-                self.board[i].append(Block())
-                self.board[i][j].set_position(x, y)
+        self._board = []
+        self._width = width
+        self._height = height
+        self._bomb_count = bomb_count
+        for i, y in enumerate(range(0, self._height*32, 32)):
+            self._board.append([])
+            for j, x in enumerate(range(0, self._width*32, 32)):
+                self._board[i].append(Block())
+                self._board[i][j].set_position(x, y)
 
         self.generate_bombs()
         self.generate_numbers()
 
+        self._game_over = None
+
     def generate_bombs(self) -> None:
-        bomb_count = self.bomb_count
-        if bomb_count > self.width * self.height:
-            bomb_count = self.width * self.height
+        bomb_count = self._bomb_count
+        if bomb_count > self._width * self._height:
+            bomb_count = self._width * self._height
         while bomb_count > 0:
-            x = random.randint(0, self.width - 1)
-            y = random.randint(0, self.height - 1)
-            if not self.board[y][x].is_bomb:
-                self.board[y][x].set_bomb()
+            x = random.randint(0, self._width - 1)
+            y = random.randint(0, self._height - 1)
+            if not self._board[y][x].is_bomb():
+                self._board[y][x].set_bomb()
                 bomb_count -= 1
 
     def generate_numbers(self) -> None:
-        for y in range(self.height):
-            for x in range(self.width):
-                if not self.board[y][x].is_bomb:
-                    self.board[y][x].set_number(self.count_bombs(x, y))
+        for y in range(self._height):
+            for x in range(self._width):
+                if not self._board[y][x].is_bomb():
+                    self._board[y][x].set_number(self.count_bombs(x, y))
 
     def count_bombs(self, x: int, y: int) -> int:
         count = 0
         for i in range(-1, 2):
             for j in range(-1, 2):
                 if (
-                    0 <= x + i < self.width
-                    and 0 <= y + j < self.height
-                    and self.board[y + j][x + i].is_bomb
+                    0 <= x + i < self._width
+                    and 0 <= y + j < self._height
+                    and self._board[y + j][x + i].is_bomb()
                 ):
                     count += 1
         return count
 
     def draw(self) -> None:
-        for y in range(self.height):
-            for x in range(self.width):
-                self.board[y][x].draw()
+        for y in range(self._height):
+            for x in range(self._width):
+                self._board[y][x].draw()
 
     def reveal(self, x: int, y: int) -> None:
-        if self.board[y][x].is_bomb:
-            self.board[y][x].set_exploded()
-            for x in range(self.width):
-                for y in range(self.height):
-                    self.board[y][x].reveal()
-        self.board[y][x].reveal()
-        if self.board[y][x].get_number() == 0:
+        if self._board[y][x].is_bomb():
+            self._board[y][x].set_exploded()
+            for x in range(self._width):
+                for y in range(self._height):
+                    self._board[y][x].reveal()
+            self._game_over = "LOSE"
+        self._board[y][x].reveal()
+        if self._board[y][x].get_number() == 0:
             for i in range(-1, 2):
                 for j in range(-1, 2):
                     if (
-                        0 <= x + i < self.width
-                        and 0 <= y + j < self.height
-                        and not self.board[y + j][x + i].is_revealed
+                        0 <= x + i < self._width
+                        and 0 <= y + j < self._height
+                        and not self._board[y + j][x + i].is_revealed()
                     ):
                         self.reveal(x + i, y + j)
+
+
+class Button:
+    def __init__(self, x: int, y: int,
+                 width: int, height: int,
+                 text: str) -> None:
+        self._x = x
+        self._y = y
+        self._width = width
+        self._height = height
+        self._text = text
+
+    def draw(self) -> None:
+        wall = pygame.Surface((self._width, self._height))
+        wall.fill((30, 30, 30))
+        screen.blit(wall, (self._x, self._y))
+        wall = pygame.transform.scale(wall, (self._width - 10,
+                                             self._height - 10))
+        wall.fill((250, 250, 250))
+        screen.blit(wall, (self._x + 5, self._y + 5))
+        font = pygame.font.SysFont("Helvetica", 20)
+        text = font.render(self._text, True, (0, 0, 0))
+        screen.blit(text, (self._x + self._width / 2 - text.get_width() / 2,
+                           self._y + self._height / 2 - text.get_height() / 2))
+
+    def is_clicked(self, pos: Tuple[int]) -> bool:
+        if pos[0] > self._x and pos[0] < self._x + self._width:
+            if pos[1] > self._y and pos[1] < self._y + self._height:
+                if pygame.mouse.get_pressed()[0]:
+                    return True
+        return False
 
 
 def main():
--- pysweeper.py
+++ pysweeper.py
@@ -1,34 +1,32 @@
 from sys import exit
 
-from datatypes import Board, pygame, screen
+from datatypes import Board, Button, pygame, screen
 
 
 class Pysweeper:
-    def __init__(self, width: int, height: int, bombs: int):
-        self.width = width
-        self.height = height
-        self.bombs = bombs
-        self.clicks = 0
-        self.flags = 0
-        self.question_marks = 0
-        self.board = Board(width, height, bombs)
+    def __init__(self, width: int, height: int, bombs: int) -> None:
+        self._width = width
+        self._height = height
+        self._bombs = bombs
+        self._clicks = 0
+        self._flags = 0
+        self._question_marks = 0
+        self._board = Board(width, height, bombs)
 
-    def run(self):
+    def run(self) -> None:
         pygame.init()
         pygame.display.set_caption("pysweeper")
         pygame.display.set_icon(pygame.image.load("textures/bomb.png"))
         clock = pygame.time.Clock()
-        pygame.display.set_mode(size=(32*self.width + 96, 32*self.height + 32))
+        pygame.display.set_mode(size=(32*self._width + 160,
+                                      32*self._height + 32),
+                                flags=pygame.RESIZABLE,
+                                vsync=True)
 
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
-
             screen.fill((255, 255, 255))
 
-            self.board.draw()
+            self._board.draw()
 
             mouse_pos = pygame.mouse.get_pos()
             mouse_x = mouse_pos[0] // 32
@@ -39,42 +37,79 @@ class Pysweeper:
                                           True,
                                           (0, 0, 0))
 
-            signal = pygame.Surface((32, 32))
-            if mouse_x < self.width and mouse_y < self.height:
-                screen.blit(m_pos_text, (32*self.width, 32*self.height))
-                if pygame.mouse.get_pressed()[0]:
-                    self.clicks += 1
-                    self.board.reveal(mouse_x, mouse_y)
-                    signal.fill((0, 255, 0))
-                    screen.blit(signal, (32*self.width + 64, 32*self.height))
-                if pygame.mouse.get_pressed()[2]:
-                    if self.board.board[mouse_y][mouse_x].is_flagged:
-                        self.flags -= 1
-                        self.board.board[mouse_y][mouse_x].unflag()
-                    else:
-                        self.flags += 1
-                        self.board.board[mouse_y][mouse_x].flag()
-                    signal.fill((255, 0, 0))
-                    screen.blit(signal, (32*self.width + 64, 32*self.height))
-                if pygame.mouse.get_pressed()[1]:
-                    if self.board.board[mouse_y][mouse_x].is_question_mark:
-                        self.question_marks -= 1
-                        self.board.board[mouse_y][mouse_x].unquestion_mark()
-                    else:
-                        self.question_marks += 1
-                        self.board.board[mouse_y][mouse_x].question_mark()
-                    signal.fill((0, 0, 255))
-                    screen.blit(signal, (32*self.width + 64, 32*self.height))
+            if mouse_x < self._width and mouse_y < self._height:
+                screen.blit(m_pos_text, (32*self._width, 32*self._height))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if mouse_x < self._width and \
+                       mouse_y < self._height and \
+                       self._board._game_over is None:
+                        block = self._board._board[mouse_y][mouse_x]
+                        signal = pygame.Surface((32, 32))
+                        if event.button == 1 and not block.is_revealed():
+                            self._clicks += 1
+                            self._board.reveal(mouse_x, mouse_y)
+                            signal.fill((0, 255, 0))
+                            screen.blit(signal, (32*self._width + 64,
+                                                 32*self._height))
+                        elif event.button == 3 and not block.is_revealed():
+                            if block.is_flagged():
+                                self._flags -= 1
+                                block.unflag()
+                            else:
+                                self._flags += 1
+                                block.flag()
+                            signal.fill((255, 0, 0))
+                            screen.blit(signal, (32*self._width + 64,
+                                                 32*self._height))
+                        elif event.button == 2:
+                            if block.is_question_mark():
+                                self._question_marks -= 1
+                                block.unquestion_mark()
+                            else:
+                                self._question_marks += 1
+                                block.question_mark()
+                            signal.fill((0, 0, 255))
+                            screen.blit(signal, (32*self._width + 64,
+                                                 32*self._height))
 
             fps_counter = text_font.render(f"FPS: {int(clock.get_fps())}",
                                            True,
                                            (0, 0, 0))
-            screen.blit(fps_counter, (32*self.width, 0))
+            screen.blit(fps_counter, (32*self._width, 0))
 
-            num_flags = text_font.render(f"Flags: {self.flags}",
+            num_flags = text_font.render(f"Flags: {self._flags}",
                                          True,
                                          (0, 0, 0))
-            screen.blit(num_flags, (32*self.width, 32))
+            screen.blit(num_flags, (32*self._width, 32))
+
+            num_clicks = text_font.render(f"Clicks: {self._clicks}",
+                                          True,
+                                          (0, 0, 0))
+            screen.blit(num_clicks, (32*self._width, 64))
+
+            num_question_marks = text_font.render(f"Question Marks: {self._question_marks}",
+                                                  True,
+                                                  (0, 0, 0))
+            screen.blit(num_question_marks, (32*self._width, 96))
+
+            if self._board._game_over == "LOSE":
+                dim_light = pygame.Surface((32*self._width, 32*self._height))
+                dim_light.set_alpha(100)
+                dim_light.fill((0, 0, 0))
+                screen.blit(dim_light, (0, 0))
+                button = Button(x=(32*self._width + 160)/2,
+                                y=(32*self._height + 32)/2,
+                                width=128,
+                                height=64,
+                                text="Restart")
+                button.draw()
+                if button.is_clicked(mouse_pos):
+                    self.__init__(self._width, self._height, self._bombs)
 
             pygame.display.update()
             clock.tick(60)
